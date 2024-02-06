91211589f80037933ad780149cabf55800f75087
karlosmartines <lund.karlmartin@gmail.com>
2024-02-06 17:45:33 +0100

    Add first RSTHD layer and #define SPLIT_USB_DETECT

--- keyboards/ferris/keymaps/karlosmartines/config.h
+++ keyboards/ferris/keymaps/karlosmartines/config.h
@@ -1,3 +1,4 @@
+#define SPLIT_USB_DETECT
 /*
 Copyright 2020 Pierre Chevalier <pierrechevalier83@gmail.com>
 
@@ -38,18 +39,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define QUICK_TAP_TERM 0
 
 // Underglow configuration
-#ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    define RGBLIGHT_EFFECT_RGB_TEST
-#    define RGBLIGHT_EFFECT_ALTERNATING
-#    define RGBLIGHT_EFFECT_TWINKLE
-#    define RGBLIGHT_HUE_STEP 8
-#    define RGBLIGHT_SAT_STEP 8
-#    define RGBLIGHT_VAL_STEP 8
-#endif
+// #ifdef RGBLIGHT_ENABLE
+// #    define RGBLIGHT_EFFECT_BREATHING
+// #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+// #    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+// #    define RGBLIGHT_EFFECT_SNAKE
+// #    define RGBLIGHT_EFFECT_KNIGHT
+// #    define RGBLIGHT_EFFECT_CHRISTMAS
+// #    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+// #    define RGBLIGHT_EFFECT_RGB_TEST
+// #    define RGBLIGHT_EFFECT_ALTERNATING
+// #    define RGBLIGHT_EFFECT_TWINKLE
+// #    define RGBLIGHT_HUE_STEP 8
+// #    define RGBLIGHT_SAT_STEP 8
+// #    define RGBLIGHT_VAL_STEP 8
+// #endif
--- keyboards/ferris/keymaps/karlosmartines/keymap.c
+++ keyboards/ferris/keymaps/karlosmartines/keymap.c
@@ -10,10 +10,10 @@ void matrix_init_user(void) {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-        KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,
-        KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,
-        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
-                          KC_1, KC_2, KC_3, KC_4
+        KC_J,       KC_C,   KC_Y,   KC_F,   KC_K,     KC_Z,   KC_L,   KC_COMM,    KC_U,       KC_Q,
+        KC_R,       KC_S,   KC_T,   KC_H,   KC_D,     KC_M,   KC_N,   KC_A,       KC_I,       KC_O,
+        KC_SLASH,   KC_V,   KC_G,   KC_P,   KC_B,     KC_X,   KC_W,   KC_DOT,     KC_SCLN,    KC_MINS,
+                                    KC_E,   KC_BSPC,  KC_ENT, KC_SPC
     )
 };
 // clang-format on
